7fec737fd5559df41609a6f925ec60d7576822aa
Eugene Hutorny <eugene@hutorny.in.ua>
2026-06-13 10:22:27 +0300

    Assume external messages are complete and allow inplace

--- test/plugin/proto23_inplace_test.cxx
+++ test/plugin/proto23_inplace_test.cxx
@@ -1,14 +1,6 @@
+#include "inplace_traits.h"
+#include <proto23_inplace_file_test.proto23.h>
 #include <proto23_inplace_test.proto23.h>
-
-template<typename T> struct is_unique_ptr : std::false_type {};
-template<typename T> struct is_unique_ptr<std::unique_ptr<T>> : std::true_type {};
-template<typename T> struct is_optional : std::false_type {};
-template<typename T> struct is_optional<std::optional<T>> : std::true_type {};
-template<typename T> struct is_inplace_vector : std::false_type {};
-template<typename T, std::size_t N> struct is_inplace_vector<proto23::inplace_vector<T, N>> : std::true_type {};
-template<typename T> struct is_inplace_string : std::false_type {};
-template<std::size_t N> struct is_inplace_string<proto23::inplace_string<N>> : std::true_type {};
-
 
 static_assert(is_unique_ptr<std::unique_ptr<long>>::value);
 static_assert(is_unique_ptr<decltype(InplaceByDefaultTest::unique_ptr_OuterPDO)>::value);
@@ -18,6 +10,7 @@ static_assert(!is_optional<decltype(InplaceByDefaultTest::inplace_inner)>::value
 static_assert(is_unique_ptr<decltype(InplaceByDefaultTest::unique_ptr_NotComplete)>::value);
 static_assert(is_optional<decltype(InplaceByDefaultTest::optional_Inner)>::value);
 static_assert(is_unique_ptr<decltype(InplaceByDefaultTest::unique_ptr_NotComplete2)>::value);
+
 static_assert(!is_optional<decltype(ExplicitInplace::inplace_Outer)>::value);
 static_assert(!is_unique_ptr<decltype(ExplicitInplace::inplace_Outer)>::value);
 static_assert(!is_optional<decltype(ExplicitInplace::inplace_Inner)>::value);
@@ -31,4 +24,24 @@ static_assert(!is_unique_ptr<decltype(ExplicitInplace::inplace_and_)>::value);
 static_assert(is_inplace_vector<decltype(ExplicitInplace::inplace_vector_Inner)>::value);
 static_assert(is_inplace_string<decltype(ExplicitInplace::inplace_string_200)>::value);
 static_assert(!is_inplace_vector<decltype(ExplicitInplace::std_vector_NotComplete)>::value);
+
+static_assert(!is_unique_ptr<decltype(InplaceByFileDefaultTest::inplace_Outer)>::value);
+static_assert(!is_unique_ptr<decltype(InplaceByFileDefaultTest::inplace_Timestamp)>::value);
+static_assert(!is_unique_ptr<decltype(InplaceByFileDefaultTest::inplace_inner)>::value);
+static_assert(is_unique_ptr<decltype(InplaceByFileDefaultTest::unique_ptr_NotComplete)>::value);
+static_assert(is_optional<decltype(InplaceByFileDefaultTest::optional_Inner)>::value);
+static_assert(is_unique_ptr<decltype(InplaceByFileDefaultTest::unique_ptr_NotComplete2)>::value);
+static_assert(is_optional<decltype(InplaceByFileDefaultTest::optional_Outer)>::value);
+static_assert(is_inplace_string<decltype(InplaceByFileDefaultTest::inplace_string)>::value);
+static_assert(is_inplace_vector<decltype(InplaceByFileDefaultTest::inplace_vector)>::value);
+
+static_assert(is_unique_ptr<decltype(ExplicitNotInplace::unique_ptr_Outer)>::value);
+static_assert(is_unique_ptr<decltype(ExplicitNotInplace::unique_ptr_Timestamp)>::value);
+static_assert(is_unique_ptr<decltype(ExplicitNotInplace::unique_ptr_Inner)>::value);
+static_assert(is_unique_ptr<decltype(ExplicitNotInplace::unique_ptr_NotComplete2)>::value);
+static_assert(is_unique_ptr<decltype(ExplicitNotInplace::unique_ptr_Inner2)>::value);
+static_assert(is_unique_ptr<decltype(ExplicitNotInplace::unique_ptr_NotComplete)>::value);
+static_assert(!is_inplace_vector<decltype(ExplicitNotInplace::vector_Inner)>::value);
+static_assert(!is_inplace_string<decltype(ExplicitNotInplace::std_string_200)>::value);
+static_assert(!is_inplace_vector<decltype(ExplicitNotInplace::std_vector_NotComplete)>::value);
 
